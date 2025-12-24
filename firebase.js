bb73d6a76544fc26a6171a5ea38dcabe943cada1
jakariya11-netjon <146520617+jakariya11-netjon@users.noreply.github.com>
2025-12-24 21:01:54 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,16 +1,14 @@
-<script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js"></script>
-<script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-database.js"></script>
+// firebase.js
 
-<script>
-  var firebaseConfig = {
-    apiKey: "AIzaSyXXXX",
-    authDomain: "badminton-live-score-a4fa8.firebaseapp.com",
-    databaseURL: "https://badminton-live-score-a4fa8-default-rtdb.firebaseio.com",
-    projectId: "badminton-live-score-a4fa8",
-    storageBucket: "badminton-live-score-a4fa8.appspot.com",
-    messagingSenderId: "XXXX",
-    appId: "XXXX"
-  };
-  firebase.initializeApp(firebaseConfig);
-</script>
+var firebaseConfig = {
+  apiKey: "AIzaSyXXXXXXXX",
+  authDomain: "badminton-live-score-a4fa8.firebaseapp.com",
+  databaseURL: "https://badminton-live-score-a4fa8-default-rtdb.firebaseio.com",
+  projectId: "badminton-live-score-a4fa8",
+  storageBucket: "badminton-live-score-a4fa8.appspot.com",
+  messagingSenderId: "XXXXXXXX",
+  appId: "XXXXXXXX"
+};
 
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
